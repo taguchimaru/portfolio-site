7edd8b1c06540002b73e1da79466c54f0d47e500
taguchimaru <104308728+taguchimaru@users.noreply.github.com>
2022-05-02 20:55:42 +0900

    SecondCommit
    動きの追加

--- js/common.js
+++ js/common.js
@@ -1,8 +1,5 @@
-/*scrollreveal*/
-ScrollReveal().reveal('.scrollrl',{
-    distance: 1500,
-    reset: true
-});
+
+/*wowの読み込み*/
 /*年号自動更新*/
 window.onload = function () {
   var nengou =　document.getElementById('yearnumber');
@@ -12,3 +9,30 @@ window.onload = function () {
   target = nengou;
   target.innerHTML = "&copy; " + year + " taguchimaru" ;
 };
+/*スムーススクロール*/
+$(function(){
+    $('a[href^="#"]').click(function(){
+      let speed =400;
+      let type ='swing';
+      let href=$(this).attr("href");
+      let target=$(href == "#index"?'html':href);
+      let position = target.offset().top;
+      $('body,html').animate({scrollTop:position},speed,type);
+      return false;
+    });
+});
+/*フェードイン*/
+$(function(){
+  $(window).scroll(function(){
+    $(".fade").each(function(){
+      var pos = $(this).offset().top;
+      var scroll =$(window).scrollTop();
+      var windowHeight =$(window).height();
+      if(scroll > pos - windowHeight + windowHeight/4){
+        $(this).addClass("fade_on");
+      }else{
+        $(this).removeClass("fade_on");
+      }
+    });
+  });
+});
